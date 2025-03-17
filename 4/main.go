2acf379a54cc34399d98d7f7be202e4f33d7ca50
Andreamjau <andreamoihasseldal@gmail.com>
2025-03-18 00:01:20 +0100

    Task 4

--- 4/main.go
+++ 4/main.go
@@ -1,3 +1,25 @@
+package main
 
+import (
+	"fmt"
+)
 
 // write a program that categorizes given number (0-10 -> small, 11-100 -> medium, 101-1000 -> large)
+
+func main(){
+	var input int
+
+	fmt.Println("Enter a number")
+	fmt.Scan(&input)
+
+	if input > 10 {
+		if input > 100 {
+			fmt.Println("Number is large")
+			return
+		}
+		fmt.Println("Number is medium")
+			return
+	}
+
+	fmt.Println("Nuber is small")
+}
